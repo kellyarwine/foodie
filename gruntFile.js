8fc0d409a0ed41bf86070244e35e8b1496f01617
Kelly Steensma <kellysteensma@gmail.com>
2014-05-24 18:43:59 -0500

    Changing grunt configurations so model.coffee from underscore doesn't get compiled

--- gruntFile.js
+++ gruntFile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
       compileScripts: {
         expand: true,
         flatten: true,
-        src: '**/*.coffee',
+        src: '*/*.coffee',
         dest: '.tmp/scripts',
         ext: '.js'
       },
@@ -28,13 +28,30 @@ module.exports = function(grunt) {
         }
       }
     },
+    jst: {
+      compile: {
+        files: {
+          '.tmp/scripts/templates.js': ['scripts/**/*.ejs']
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.css',
+        options: {
+          livereload: true,
+        },
+      },
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-web-server');
+  grunt.loadNpmTasks('grunt-contrib-jst');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('compile', ['clean', 'coffee']);
   grunt.registerTask('build', ['clean', 'coffee']);
-  grunt.registerTask('default', ['clean', 'coffee', 'web_server']);
+  grunt.registerTask('default', ['clean', 'coffee', 'jst', 'web_server', 'watch']);
 }
